fbf3dac31a8399229c6ec90e2fe015ca6f56d7ad
admin <iliassmilodon@gmail.com>
2025-10-17 18:51:38 +0100

    first commit

---
+++ src/main/java/com/devops/pipeline_v1/PipelineV1Application.java
@@ -0,0 +1,13 @@
+package com.devops.pipeline_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/pipeline_v1/PipelineV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.pipeline_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
