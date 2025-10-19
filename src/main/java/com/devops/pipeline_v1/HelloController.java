5021009333224277b1dcd18144ae0a14f7297c13
admin <iliassmilodon@gmail.com>
2025-10-19 17:20:27 +0100

    adding the monitoring api

--- src/main/java/com/devops/pipeline_v1/HelloController.java
+++ src/main/java/com/devops/pipeline_v1/HelloController.java
@@ -1,5 +1,9 @@
 package com.devops.pipeline_v1;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +20,16 @@ public class HelloController {
     @GetMapping("/status")
     public String status() {
         return "Application is running successfully!";
+    }
+
+
+      // Endpoint Health pour monitoring
+    @GetMapping("/health")
+    public Map<String, Object> health() {
+        Map<String, Object> health = new HashMap<>();
+        health.put("status", "UP");
+        health.put("timestamp", LocalDateTime.now().toString());
+        health.put("application", "demo-app");
+        return health;
     }
 }
